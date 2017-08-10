85e2a288b6b3f500fc868ff4d35f0937cebaa477
yarden hochman <yyyarden@gmail.com>
2017-08-10 14:38:54 -0400

    random sequence now flashes

--- script/Game.js
+++ script/Game.js
@@ -4,12 +4,14 @@ function Game() {
                               //generates a sequence and builds ui elements
   this.player = new Player("You"); //in the future will add a method to take player name when
                               //multiplayeris enabled
-  this.gameplay = new GamePlay();     //the actual gameplay happens here until it's game over
+  const currentGame = this;
+  this.gamePlay = new GamePlay(currentGame);     //the actual gameplay happens here until it's game over
   this.announce = new Announcements();
                               //will create announcements based on results
   this.play = function(){
     this.colorSequence.sequenceGenerator();
     //this.colorSequence.createButtons(); to be defined
+    this.gamePlay.flash();
     //do {
       //this.gamePlay.flash();
       //this.gamePlay.playerSelects();
--- script/GamePlay.js
+++ script/GamePlay.js
@@ -1,19 +1,29 @@
-function GamePlay(){ //flash , player clicks
-	/*this.continuation = function(){
-		while (this.player.currentSelection-1<this.colorSequence.sizeOfSequence){
-			this.flash();
-			this.playerSelects();
-			this.compare();
-      this.player.sequenceLength++;
-		}
-	}*/
+function GamePlay(currentGame){ //flash , player clicks
+
 	this.playerSelects = function(){
 		//code listening for player clicks. the function will store the player selection in
 		//game.player.currentSelection array
 		//once this array is the same length as game.player.sequenceLength the function stops
+    $("#red")
 	}
 	this.flash = function(){
 			//code manipulating element to display flashes according to the this.player.sequenceLength
+      var count = 0;
+      for (let i = 0 ; i < currentGame.player.sequenceLength ; i++){
+        let lightToFlash='#'+currentGame.colorSequence.theSequence[i];
+        let currentButton = $(lightToFlash);
+        setTimeout(function(){
+          currentButton.addClass('flash');
+          setTimeout(function(){
+          currentButton.removeClass('flash');
+        } , 250); //dark delay
+
+
+        } , count*400); //lighting delay
+        count++;
+
+      }
+
 	}
 	this.match = function(){
 		//runs after the playerSelection is done, checks if the arrays match
