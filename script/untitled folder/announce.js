08c909554f66b68d2f2d9e1656a6c413a11c59cf
yarden hochman <yyyarden@gmail.com>
2017-08-14 09:43:49 -0400

    changed initial sequence to 2 steps

--- script/untitled folder/announce.js
+++
@@ -1,7 +0,0 @@
-function Announcements(currentPlayer){
-    if (currentPlayer.sequenceLength===2){
-      alert(`Game Over! $[player.name] couldn't remember a sequence of 5 colors`);
-      return;
-    }
-    alert(`Game Over! $[player.name] remembered $[this.player.sequenceLength]`);
-}
